238243536d468041eb40b6c3efd31b26742d437b
Joshua Rice <jrice@lacrossefootwear.com>
2018-02-12 09:43:43 -0800

    docs

--- app/code/community/Rice/EnhancedAdminNotification/Model/Resource/Inbox.php
+++ app/code/community/Rice/EnhancedAdminNotification/Model/Resource/Inbox.php
@@ -3,14 +3,14 @@
 class Rice_EnhancedAdminNotification_Model_Resource_Inbox extends Mage_AdminNotification_Model_Resource_Inbox
 {
 	/**
-	 * Overridden to automatically mark incoming notices as read if desired.
+	 * Overridden to automatically mark incoming notices as read if needed.
 	 * It does not seem like Magento writes to this table using the ORM, but just in case...
 	 * @param Mage_Core_Model_Abstract $object
 	 * @return Mage_Core_Model_Resource_Db_Abstract
 	 */
 	protected function _beforeSave(Mage_Core_Model_Abstract $object)
 	{
-		if ($isNoticeSuppressed = Mage::getStoreConfigFlag('system/adminnotification/hide_notices')) {
+		if (Mage::getStoreConfigFlag('system/adminnotification/hide_notices')) {
 			$object->setData('is_read', 1);
 		}
 		return parent::_beforeSave($object);
