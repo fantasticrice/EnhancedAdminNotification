0a0cf8135ba494ca2ebd82025fe7018d0a455a4a
Joshua Rice <jrice@lacrossefootwear.com>
2018-02-11 16:35:48 -0800

    Override where Magento actually inserts new messages

--- app/code/community/Rice/EnhancedAdminNotification/Model/Resource/Inbox.php
+++ app/code/community/Rice/EnhancedAdminNotification/Model/Resource/Inbox.php
@@ -3,17 +3,33 @@
 class Rice_EnhancedAdminNotification_Model_Resource_Inbox extends Mage_AdminNotification_Model_Resource_Inbox
 {
 	/**
-	 * Perform actions before object save
 	 * Overridden to automatically mark incoming notices as read if desired.
+	 * It does not seem like Magento writes to this table using the ORM, but just in case...
 	 * @param Mage_Core_Model_Abstract $object
 	 * @return Mage_Core_Model_Resource_Db_Abstract
 	 */
 	protected function _beforeSave(Mage_Core_Model_Abstract $object)
 	{
-		// TODO send a fake incoming message with notice level severity
 		if ($isNoticeSuppressed = Mage::getStoreConfigFlag('system/adminnotification/hide_notices')) {
 			$object->setData('is_read', 1);
 		}
 		return parent::_beforeSave($object);
+	}
+
+	/**
+	 * Overridden to automatically mark incoming notices as read if needed.
+	 * @param Mage_AdminNotification_Model_Inbox $object
+	 * @param array $data
+	 */
+	public function parse(Mage_AdminNotification_Model_Inbox $object, array $data)
+	{
+		if (Mage::getStoreConfigFlag('system/adminnotification/hide_notices')) {
+			foreach ($data as $key => $item) {
+				if (isset($item['severity']) && $item['severity'] == Mage_AdminNotification_Model_Inbox::SEVERITY_NOTICE) {
+					$data[$key]['is_read'] = 1;
+				}
+			}
+		}
+		parent::parse($object, $data);
 	}
 }
